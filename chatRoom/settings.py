9e86b407fcacf9e1a1af66b9fe185bd071bce902
Victor E <victorevector@gmail.com>
2015-05-12 17:01:40 -0700

    Initialized static files

--- chatRoom/settings.py
+++ chatRoom/settings.py
@@ -13,6 +13,8 @@ import os
 BASE_DIR = os.path.dirname(os.path.dirname(__file__))
 TEMPLATE_PATH = os.path.join(BASE_DIR, 'templates')
 TEMPLATE_DIRS = [TEMPLATE_PATH]
+STATIC_PATH = os.path.join(BASE_DIR, 'static')
+STATICFILES_DIRS = ( STATIC_PATH,)
 
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/1.7/howto/deployment/checklist/
