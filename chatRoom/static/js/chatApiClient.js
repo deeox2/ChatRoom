b84389fe6a3ea8059f5d8f1d56c13b23a9a6193d
Victor E <victorevector@gmail.com>
2015-05-18 16:46:22 -0700

    forgot to change the URL bak to production URL

--- chatRoom/static/js/chatApiClient.js
+++ chatRoom/static/js/chatApiClient.js
@@ -1,7 +1,6 @@
 var chatClientModule = window.chatRoomApiNameSpace | {} ;
 var chatClientModule = function(){
-    var URL = 'http://127.0.0.1:8000/chatroom/api';
-    // 'https://morning-headland-7475.herokuapp.com/chatroom/api';
+    var URL = 'https://morning-headland-7475.herokuapp.com/chatroom/api';
     return { main: main};
 
     function main(){
